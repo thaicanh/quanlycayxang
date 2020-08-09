4fe2bfc8d9cb5445da8316043b9e5bc6ad6eb646
thaicanh <69358841+thaicanh@users.noreply.github.com>
2020-08-09 23:41:19 +0700

    Update getData.php

--- getData.php
+++ getData.php
@@ -1,16 +1,18 @@
 <?php 
+session_start();
+    $hostName="sql12.freemysqlhosting.net";
+    $username="sql12359268";
+    $pass ="lBI48EpC4d";
+    $dbName ="sql12359268";
 
-	
-    $hostName="localhost";
-    $username="root";
-    $pass ="";
-    $dbName ="quanlicayxang";
-  	$ketnoi = mysqli_connect($hostName,$username ,$pass,$dbName) or die("khong the ket noi");
-  	if (isset($_GET['id'])) {
-  		$idsensor = $_GET['id'];
-  		$query = mysqli_query($ketnoi,"SELECT * FROM sensor JOIN value ON (sensor.idsensor=value.idsensor) WHERE value.idsensor = '$idsensor' ORDER BY  time DESC limit 1");
-    	$sensor = mysqli_fetch_array($query);	
-    	echo json_encode($sensor);
-  	}
- 	
+    if (!array_key_exists("usrname", $_SESSION)) {
+      header("location:login.php");
+    }
+    if (isset($_POST['logout'])) {
+      unset($_SESSION['usrname']);
+       // print_r($_SESSION);
+      header("location:login.php");
+    }
+ 
+  $ketnoi = mysqli_connect($hostName,$username ,$pass,$dbName) or die("khong the ket noi");
  ?>
